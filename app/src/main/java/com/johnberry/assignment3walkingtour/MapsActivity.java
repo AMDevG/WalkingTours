0b97248c34f9fb05a0c0602c2dfcda501f495034
Nighthawk <jb@Nighthawks-MBP.attlocal.net>
2021-02-22 15:10:27 -0600

    Showing Geofences,Notifications...No marker

--- app/src/main/java/com/johnberry/assignment3walkingtour/MapsActivity.java
+++ app/src/main/java/com/johnberry/assignment3walkingtour/MapsActivity.java
@@ -1,40 +1,109 @@
 package com.johnberry.assignment3walkingtour;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
+import com.google.android.gms.tasks.Task;
 import com.johnberry.assignment3walkingtour.databinding.ActivityMapsBinding;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    // Needs:
+    //      implementation 'com.google.android.gms:play-services-maps:17.0.0'
+    //      implementation 'com.google.android.gms:play-services-location:17.0.0'
+
+    //      android:usesCleartextTraffic="true"
+
+    private static final String TAG = "MapsActivity";
+
     private GoogleMap mMap;
-    private ActivityMapsBinding binding;
+    private static final int LOCATION_REQUEST = 111;
+    private static final int ACCURACY_REQUEST = 222;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private Polyline llHistoryPolyline;
+    private final ArrayList<LatLng> latLonHistory = new ArrayList<>();
+    private boolean zooming = false;
+    private float oldZoom;
+    private Marker carMarker;
+    private FenceMgr fenceMgr;
+    private Geocoder geocoder;
+    private TextView addressText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
 
-        binding = ActivityMapsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+//        addressText = findViewById(R.id.addressText);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        checkLocationAccuracy();
+        checkPermission();
+
+        geocoder = new Geocoder(this);
     }
 
+    public void initMap() {
+
+        fenceMgr = new FenceMgr(this);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+
     /**
-     * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -42,14 +111,255 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(16.0f));
+        zooming = true;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setBuildingsEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setIndoorLevelPickerEnabled(true);
+
+        if (checkPermission()) {
+            setupLocationListener();
+            setupZoomListener();
+        }
+
+
+    }
+
+
+    private boolean checkPermission() {
+        ArrayList<String> perms = new ArrayList<>();
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            perms.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                perms.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION);
+            }
+        }
+
+        if (!perms.isEmpty()) {
+            String[] array = perms.toArray(new String[0]);
+            ActivityCompat.requestPermissions(this,
+                    array, LOCATION_REQUEST);
+            return false;
+        }
+
+
+        return true;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == LOCATION_REQUEST) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (permissions[i].equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        setupLocationListener();
+                        setupZoomListener();
+                    } else {
+                        Toast.makeText(this, "Location Permission not Granted", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    }
+
+    private void setupLocationListener() {
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new MyLocListener(this);
+
+        //minTime	    long: minimum time interval between location updates, in milliseconds
+        //minDistance	float: minimum distance between location updates, in meters
+        if (checkPermission() && locationManager != null)
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, locationListener);
+
+
+    }
+
+    public void showClicked(View v) {
+        CheckBox cb = (CheckBox) v;
+
+        if (cb.isChecked()) {
+            fenceMgr.drawFences();
+        } else {
+            fenceMgr.eraseFences();
+        }
+
     }
 
     public GoogleMap getMap() {
         return mMap;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (locationManager != null && locationListener != null)
+            locationManager.removeUpdates(locationListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (checkPermission() && locationManager != null && locationListener != null)
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 10, locationListener);
+    }
+
+    private void setupZoomListener() {
+        mMap.setOnCameraIdleListener(() -> {
+            if (zooming) {
+                Log.d(TAG, "onCameraIdle: DONE ZOOMING: " + mMap.getCameraPosition().zoom);
+                zooming = false;
+                oldZoom = mMap.getCameraPosition().zoom;
+            }
+        });
+
+        mMap.setOnCameraMoveListener(() -> {
+            if (mMap.getCameraPosition().zoom != oldZoom) {
+                Log.d(TAG, "onCameraMove: ZOOMING: " + mMap.getCameraPosition().zoom);
+                zooming = true;
+            }
+        });
+    }
+
+    public void updateLocation(Location location) {
+
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        latLonHistory.add(latLng); // Add the LL to our location history
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            Address address = addresses.get(0);
+            addressText.setText(address.getAddressLine(0));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            addressText.setText("");
+        }
+
+
+        if (llHistoryPolyline != null) {
+            llHistoryPolyline.remove(); // Remove old polyline
+        }
+
+        if (latLonHistory.size() == 1) { // First update
+            mMap.addMarker(new MarkerOptions().alpha(0.5f).position(latLng).title("My Origin"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15.0f));
+            zooming = true;
+            return;
+        }
+
+        if (latLonHistory.size() > 1) { // Second (or more) update
+            PolylineOptions polylineOptions = new PolylineOptions();
+
+            for (LatLng ll : latLonHistory) {
+                polylineOptions.add(ll);
+            }
+            llHistoryPolyline = mMap.addPolyline(polylineOptions);
+            llHistoryPolyline.setEndCap(new RoundCap());
+            llHistoryPolyline.setWidth(8);
+            llHistoryPolyline.setColor(Color.BLUE);
+
+
+            float r = getRadius();
+            if (r > 0) {
+                Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.walker_left);
+                Bitmap resized = Bitmap.createScaledBitmap(icon, (int) r, (int) r, false);
+
+                BitmapDescriptor iconBitmap = BitmapDescriptorFactory.fromBitmap(resized);
+
+                MarkerOptions options = new MarkerOptions();
+                options.position(latLng);
+                options.icon(iconBitmap);
+                options.rotation(location.getBearing());
+
+                if (carMarker != null) {
+                    carMarker.remove();
+                }
+
+                carMarker = mMap.addMarker(options);
+            }
+        }
+
+        if (!zooming)
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+
+    }
+
+    private float getRadius() {
+        float z = mMap.getCameraPosition().zoom;
+        return 15f * z - 145f;
+    }
+
+
+    private void checkLocationAccuracy() {
+
+        Log.d(TAG, "checkLocationAccuracy: ");
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, locationSettingsResponse -> {
+            Log.d(TAG, "onSuccess: High Accuracy Already Present");
+            initMap();
+        });
+
+        task.addOnFailureListener(this, e -> {
+            if (e instanceof ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    ResolvableApiException resolvable = (ResolvableApiException) e;
+                    resolvable.startResolutionForResult(MapsActivity.this, ACCURACY_REQUEST);
+                } catch (IntentSender.SendIntentException sendEx) {
+                    sendEx.printStackTrace();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ACCURACY_REQUEST && resultCode == RESULT_OK) {
+            Log.d(TAG, "onActivityResult: ");
+            initMap();
+        } else {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("High-Accuracy Location Services Required");
+            builder.setMessage("High-Accuracy Location Services Required");
+            builder.setPositiveButton("OK", (dialog, id) -> finish());
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+        }
     }
 }
