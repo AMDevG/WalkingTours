f8d34833ef747b10e0ff45eb5f4fcb7f8358c4f1
Nighthawk <jb@Nighthawks-MBP.attlocal.net>
2021-03-03 14:42:23 -0600

    Notification Activity complete; added TypeFace

--- app/src/main/java/com/johnberry/assignment3walkingtour/NotificationActivity.java
+++ app/src/main/java/com/johnberry/assignment3walkingtour/NotificationActivity.java
@@ -1,12 +1,15 @@
 package com.johnberry.assignment3walkingtour;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -20,24 +23,37 @@ import java.util.Locale;
 public class NotificationActivity extends Activity {
     private FenceData selectedFence;
     private TextView titleTextView;
-    private TextView addressTextView;
+    private TextView addressTextView, descriptText;
     private ImageView buildingImage;
+    private Typeface myCustomFont;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        myCustomFont = Typeface.createFromAsset(getAssets(), "fonts/Acme-Regular.ttf");
 
         Intent i = getIntent();
         selectedFence = (FenceData) i.getSerializableExtra("FD");
 
+
+
         titleTextView = findViewById(R.id.titleText);
         titleTextView.setText(selectedFence.getId());
 
+        addressTextView = findViewById(R.id.addrTextView);
+        addressTextView.setText(selectedFence.getAddress());
+
+        descriptText = findViewById(R.id.descripText);
+        descriptText.setText(selectedFence.getDescription());
+
         buildingImage = findViewById(R.id.buildingImage);
 
-        System.out.println("Here is the title: " + selectedFence.getId());
+        titleTextView.setTypeface(myCustomFont);
+        addressTextView.setTypeface(myCustomFont);
+        descriptText.setTypeface(myCustomFont);
 
         loadImage();
     }
